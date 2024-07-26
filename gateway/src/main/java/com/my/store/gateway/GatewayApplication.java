806dadb7ce64838df0fe807dbc2a3c01edfe394b
Tan Hao Nguyen <101847895+tanhaok@users.noreply.github.com>
2024-07-26 20:16:37 +0700

    init: add detail for each components (#1)
    * init: add detail for each components
    
    * init: add base web for java #2
    
    * init: add ui component
    
    * init: add go-gin base project #2
    
    * init: add fastapi python project #2
    
    * init: add docker compose file & services needed

---
+++ gateway/src/main/java/com/my/store/gateway/GatewayApplication.java
@@ -0,0 +1,13 @@
+package com.my.store.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+}
