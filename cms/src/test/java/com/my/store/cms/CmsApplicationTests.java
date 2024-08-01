55450a6bf75456f2a51301163b856d61e52c5720
harold <haonguyentan2001@gmail.com>
2024-08-01 19:58:55 +0700

    fix: remove unrelated code

--- cms/src/test/java/com/my/store/cms/CmsApplicationTests.java
+++ cms/src/test/java/com/my/store/cms/CmsApplicationTests.java
@@ -1,8 +1,13 @@
 package com.my.store.cms;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
 
 }
