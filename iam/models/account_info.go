620a80eb1d1466e7d4ca40aaa36eb2716b79907e
harold <haonguyentan2001@gmail.com>
2024-08-23 21:27:09 +0700

    MS-Main: test webhook

--- iam/models/account_info.go
+++ iam/models/account_info.go
@@ -4,8 +4,8 @@ package models
 type AccountInfo struct {
 	ID               string `json:"id"`
 	AccountId        string `json:"account_id"`
-	FirstName        string `json:"firstname"`
-	LastName         string `json:"lastname"`
+	FirstName        string `json:"first_name"`
+	LastName         string `json:"last_name"`
 	PhoneNumber      string `json:"phone_number"`
 	PrimaryAddress   string `json:"primary_address"`
 	SecondaryAddress string `json:"secondary_address"`
